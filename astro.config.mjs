bee9afb3c72aa3f353e99f048405d55ae699dd20
unknown <kakarotoroth@gmail.com>
2023-06-21 18:11:18 -0400

    9

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,9 +7,11 @@ export default defineConfig({
   integrations: [tailwind()],
   output: 'static',
   outDir: './docs',
-  base:'astro-proyect',
+  // base:'astro-proyect',
   build: {
     // Example: Generate `page.html` instead of `page/index.html` during build.
-    format: 'file'
+    format: 'file',
+    assets: 'assests',
+    assetsPrefix: '.'
   }
 });
