320a6e86e44220f69c9d5f06841e585560c02c19
unknown <kakarotoroth@gmail.com>
2023-06-21 16:05:06 -0400

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,11 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind()],
+  output: 'static',
+  site:'http://localhost:3000/',
+  base:'dist'
+});
